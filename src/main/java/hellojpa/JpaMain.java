bca58a333086849a09fb5a2e5c4637d81a546f54
sedna99 <ljsmmm1228@gmail.com>
2024-04-14 23:53:03 +0900

    섹션2: Hello JPA - 어플리케이션 개발 - 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
