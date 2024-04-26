2440f33dc81f1b7a1dee1dc7229fbe2161ea4ba7
sedna99 <ljsmmm1228@gmail.com>
2024-04-26 22:50:27 +0900

    섹션10: 소개

--- jpashop/src/main/java/org/jpabook/JpaMain.java
+++ jpashop/src/main/java/org/jpabook/JpaMain.java
@@ -1,6 +1,9 @@
 package org.jpabook;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.jpabook.jpashop.Book;
 import org.jpabook.jpashop.Member;
 import org.jpabook.jpashop.Order;
@@ -13,15 +16,39 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
+        Member member1 = new Member();
+        member1.setName("kim");
+        em.persist(member1);
+
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            List<Member> members = em.createQuery("select m from Member m where m.name" +
+                            " like '%kim'", Member.class)
+                    .getResultList();
+            for (Member member : members) {
+                System.out.println("member = " + member.getName());
+            }
 
-             Book book = new Book();
-             book.setName("JPA");
-             book.setAuthor("kim");
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+            Root<Member> m = query.from(Member.class);
 
-             em.persist(book);
+            String username = "kim";
+            if (username != null) {
+                query = query.where(cb.equal(m.get("name"), "kim"));
+            }
+
+            query.select(m).where(cb.equal(m.get("name"), "kim"));
+            List<Member> members2 = em.createQuery(query).getResultList();
+            for (Member member : members2) {
+                System.out.println("member = " + member.getName());
+            }
+
+            List<Member> memberList = em.createNativeQuery("select * from member", Member.class).getResultList();
+            for (Member member : memberList) {
+                System.out.println("member = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
