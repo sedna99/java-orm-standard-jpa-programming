b0e5fafb9b8e0596b50679ece5dbf10658307619
sedna99 <ljsmmm1228@gmail.com>
2024-04-26 22:24:52 +0900

    섹션9: 실전 예제 6 - 값 타입 매핑

--- jpashop/src/main/java/org/jpabook/jpashop/Delivery.java
+++ jpashop/src/main/java/org/jpabook/jpashop/Delivery.java
@@ -1,5 +1,6 @@
 package org.jpabook.jpashop;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,8 +10,31 @@ public class Delivery extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String string;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
     private DeliveryStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
 }
--- jpashop/src/main/java/org/jpabook/jpashop/Member.java
+++ jpashop/src/main/java/org/jpabook/jpashop/Member.java
@@ -11,9 +11,9 @@ public class Member extends BaseEntity{
     @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
-    private String city;
-    private String street;
-    private String zipcode;
+
+    @Embedded
+    private Address homeAddress;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
@@ -26,18 +26,6 @@ public class Member extends BaseEntity{
         this.name = name;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public Long getId() {
         return id;
     }
@@ -46,15 +34,19 @@ public class Member extends BaseEntity{
         return name;
     }
 
-    public String getCity() {
-        return city;
+    public Address getHomeAddress() {
+        return homeAddress;
     }
 
-    public String getStreet() {
-        return street;
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 
-    public String getZipcode() {
-        return zipcode;
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 }
