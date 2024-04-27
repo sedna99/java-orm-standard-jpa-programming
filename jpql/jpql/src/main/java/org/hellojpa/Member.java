b0074b2f55dc5d0298cb9758004134ef5e3595f0
sedna99 <ljsmmm1228@gmail.com>
2024-04-27 17:37:43 +0900

    섹션10: 기본 문법과 쿼리 API

---
+++ jpql/jpql/src/main/java/org/hellojpa/Member.java
@@ -0,0 +1,49 @@
+package org.hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private int age;
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
