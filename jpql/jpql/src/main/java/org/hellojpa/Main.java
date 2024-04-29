f07f76d59d054a54d2b606a6ceeaab3883e2ed1a
sedna99 <ljsmmm1228@gmail.com>
2024-04-29 23:19:46 +0900

    섹션10: JPQL 함수

--- jpql/jpql/src/main/java/org/hellojpa/Main.java
+++ jpql/jpql/src/main/java/org/hellojpa/Main.java
@@ -23,32 +23,70 @@ public class Main {
             member.setMemberType(MemberType.USER);
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setAge(10);
+            member2.setTeam(team);
+            member2.setMemberType(MemberType.USER);
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-            String query = "select " +
-                    "case " +
-                        "when age <= 10 then '학생요금' " +
-                        "when age >= 60 then '경로요금' " +
-                        "else '일반요금' " +
-                    "end " +
-                    "from Member m";
+            String query = "select concat('a', 'b') from Member m";
             List<String> result = em.createQuery(query, String.class).getResultList();
             for (String s : result) {
                 System.out.println("s = " + s);
             }
 
-            String query2 = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            String query2 = "select substring(m.username, 2, 3) from Member m";
             List<String> result2 = em.createQuery(query2, String.class).getResultList();
             for (String s : result2) {
                 System.out.println("s = " + s);
             }
 
-            String query3 = "select nullif(m.username, '관리자') from Member m";
+            String query3 = "select trim('  abc  ') from Member m";
             List<String> result3 = em.createQuery(query3, String.class).getResultList();
             for (String s : result3) {
                 System.out.println("s = " + s);
             }
+
+            String query4 = "select lower('ABC') from Member m";
+            List<String> result4 = em.createQuery(query4, String.class).getResultList();
+            for (String s : result4) {
+                System.out.println("s = " + s);
+            }
+
+            String query5 = "select locate('de', 'abcdefg') from Member m";
+            List<Integer> result5 = em.createQuery(query5, Integer.class).getResultList();
+            for (Integer s : result5) {
+                System.out.println("s = " + s);
+            }
+
+            String query6 = "select ABS(-1) from Member m";
+            List<Integer> result6 = em.createQuery(query6, Integer.class).getResultList();
+            for (Integer s : result6) {
+                System.out.println("s = " + s);
+            }
+
+            String query7 = "select length('abc') from Member m";
+            List<Integer> result7 = em.createQuery(query7, Integer.class).getResultList();
+            for (Integer s : result7) {
+                System.out.println("s = " + s);
+            }
+
+            String query8 = "select size(t.members) from Team t";
+            List<Integer> result8 = em.createQuery(query8, Integer.class).getResultList();
+            for (Integer s : result8) {
+                System.out.println("s = " + s);
+            }
+
+//            index()는 값 컬렉션의 인덱스를 조회할 때 사용
+//            String query9 = "select index(t.members) from Team t";
+//            List<Integer> result9 = em.createQuery(query9, Integer.class).getResultList();
+//            for (Integer s : result9) {
+//                System.out.println("s = " + s);
+//            }
 
             tx.commit();
         } catch (Exception e) {
