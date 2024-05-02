38a4cdcdeddd49b68f822075ff4e00311c1ee814
sedna99 <ljsmmm1228@gmail.com>
2024-05-02 22:03:41 +0900

    섹션11: 경로표현식 - 3

--- jpql/jpql/src/main/java/org/hellojpa/Main.java
+++ jpql/jpql/src/main/java/org/hellojpa/Main.java
@@ -12,6 +12,31 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            member1.setMemberType(MemberType.ADMIN);
+            member1.setTeam(team);
+
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setMemberType(MemberType.ADMIN);
+            member2.setTeam(team);
+
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m.username from Team t join t.members m";
+            em.createQuery(query, List.class)
+                    .getResultList()
+                    .forEach(System.out::println);
 
             tx.commit();
         } catch (Exception e) {
